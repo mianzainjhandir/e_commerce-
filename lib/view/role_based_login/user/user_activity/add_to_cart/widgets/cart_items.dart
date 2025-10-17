59d5026939fd52d6f96fa5e251856c63b97e69fc
Bilal Saeed <mianzainjh@gmail.com>
2025-10-17 17:30:45 +0500

    designing the screen of cart items.

--- lib/view/role_based_login/user/user_activity/add_to_cart/widgets/cart_items.dart
+++ lib/view/role_based_login/user/user_activity/add_to_cart/widgets/cart_items.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:explorer/core/common/utils/color_conversation.dart';
 import 'package:explorer/view/role_based_login/user/user_activity/model/cart_model.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class CartItems extends StatelessWidget {
   final CartModel cart;
@@ -25,10 +27,36 @@ class CartItems extends StatelessWidget {
               height: 120,
               width: 100,
               fit: BoxFit.cover,
-            )
-        ]
-          ,)
-      ],),
+            ),
+            SizedBox(width: 20,),
+            Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(cart.productData['name'],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),),
+                    SizedBox(height: 5,),
+                    Row(
+                      children: [
+                        Text("Color:"),
+                        Gap(5),
+                        CircleAvatar(radius: 10, backgroundColor: getColorFromName(cart.selectedColor),)
+                      ],
+                    )
+                  ],
+                ),
+            ),
+        ],
+        ),
+      ],
+      ),
     );
   }
 }
