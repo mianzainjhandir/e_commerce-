549564c8c2ea4e3fdb3a32b4785a9d1eca2276fa
Bilal Saeed <mianzainjh@gmail.com>
2025-10-30 20:53:44 +0500

    add  dialog confirm detail and feature for add address...

---
+++ lib/view/role_based_login/user/screens/user_profile/payment/add_payment_methods.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AddPaymentMethods extends StatefulWidget {
+  const AddPaymentMethods({super.key});
+
+  @override
+  State<AddPaymentMethods> createState() => _AddPaymentMethodsState();
+}
+
+class _AddPaymentMethodsState extends State<AddPaymentMethods> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
