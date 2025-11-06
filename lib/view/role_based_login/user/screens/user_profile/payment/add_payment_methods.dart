b1b6d852ce0c20e24de67875bffb1c05484b0932
Bilal Saeed <mianzainjh@gmail.com>
2025-11-06 21:25:54 +0500

    working on payment method.

--- lib/view/role_based_login/user/screens/user_profile/payment/add_payment_methods.dart
+++ lib/view/role_based_login/user/screens/user_profile/payment/add_payment_methods.dart
@@ -1,4 +1,6 @@
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddPaymentMethods extends StatefulWidget {
@@ -11,7 +13,15 @@ class AddPaymentMethods extends StatefulWidget {
 //.......lllll
 
 class _AddPaymentMethodsState extends State<AddPaymentMethods> {
+  String? selectedPaymentSystem;
   final _formKey = GlobalKey<FormState>();
+  Future<List<Map<String, dynamic>>> fetchPaymentSystem () async{
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("payment_methods").get();
+    return snapshot.docs.map((doc) => {
+      'name' : doc['name'],
+      'image': doc['image'],
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +32,40 @@ class _AddPaymentMethodsState extends State<AddPaymentMethods> {
       ),
       body: SafeArea(child: Padding(padding: EdgeInsets.symmetric(horizontal: 15),
       
-      child: Form(child: Column(
+      child: Form(
+          key: _formKey,
+          child: Column(
         children: [
-
+          FutureBuilder(
+              future: fetchPaymentSystem(),
+              builder: (context,snapshot){
+                if(snapshot.hasError){
+                  return Text("Error: ${snapshot.error}");
+                }else if(!snapshot.hasData || snapshot.data!.isEmpty){
+                  return const Text("No Payment system available");
+                }
+                return DropdownButton<String>(
+                  elevation: 2,
+                    value: selectedPaymentSystem,
+                    hint: Text("Select Payment System"),
+                    items: snapshot.data!.map((system){
+                      return DropdownMenuItem<String>(
+                          value: system['name'],
+                          child: Row(
+                            children: [
+                              CachedNetworkImage(imageUrl: system['image'],
+                                width: 30,height: 30,errorWidget: (context, stackTrace, error) => Icon(
+                                    Icons.error),
+                              ),
+                              SizedBox(width: 10,),
+                              Text(system['name'])
+                            ],
+                          ));}).toList(),
+                    onChanged: (value){
+                    selectedPaymentSystem = value;
+                    // selectedPaymentSystem = snapshot.data!.firstWhere(test)
+                    });
+              })
         ],
       )),)),
     );
